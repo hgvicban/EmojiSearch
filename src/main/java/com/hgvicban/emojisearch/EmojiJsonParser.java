168aef278b0efd479c15f9f514725086d2b732ad
Grace Icban <hgvicban@gmail.com>
2021-03-08 14:27:14 -0500

    add EmojiJsonParser

---
+++ src/main/java/com/hgvicban/emojisearch/EmojiJsonParser.java
@@ -0,0 +1,4 @@
+package com.hgvicban.emojisearch;
+
+public class EmojiJsonParser {
+}
---
+++ src/test/java/com/hgvicban/emojisearch/EmojiJsonParserTest.java
@@ -0,0 +1,11 @@
+package com.hgvicban.emojisearch;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EmojiJsonParserTest {
+    @Test
+    void ok() {
+        assertTrue(true);
+    }
+}
