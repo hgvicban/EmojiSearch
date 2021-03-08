621f3679a50b4293f658ba6ac84313bb84b283a2
Grace Icban <hgvicban@gmail.com>
2021-03-08 14:17:19 -0500

    Initial commit

---
+++ src/main/java/com/hgvicban/emojisearch/EmojisearchApplication.java
@@ -0,0 +1,13 @@
+package com.hgvicban.emojisearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmojisearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmojisearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hgvicban/emojisearch/EmojisearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgvicban.emojisearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmojisearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
