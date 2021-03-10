c9fe60f91a0e35325c7ce2431afd0a182bd56607
Grace Icban <hgvicban@gmail.com>
2021-03-10 15:16:49 -0500

    rename class

--- src/main/java/com/hgvicban/parser/EmojiSearchApplication.java
+++ src/main/java/com/hgvicban/parser/EmojiSearchApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmojisearchApplication {
+public class EmojiSearchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmojisearchApplication.class, args);
+        SpringApplication.run(EmojiSearchApplication.class, args);
+        System.out.println("Hello world!");
     }
 
 }
--- src/test/java/com/hgvicban/parser/EmojiSearchApplicationTests.java
+++ src/test/java/com/hgvicban/parser/EmojiSearchApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmojisearchApplicationTests {
+class EmojiSearchApplicationTests {
 
     @Test
     void contextLoads() {
